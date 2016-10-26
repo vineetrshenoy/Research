3987ebdd7eb586eed9b2f4cd189eea4a9cc7e44c
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2016-10-26 12:03:57 -0400

    Problem with user number

--- ML/simpleNorm.m
+++ ML/simpleNorm.m
@@ -1,10 +1,11 @@
-function [accurary] = simpleNorm(fullMatrix)
+function [accuracy] = simpleNorm(fullMatrix)
 %This function takes a full data matrix, creates a 
 %testing and training set, and computes a simplie norm error,
 %It averages the strokes for a user in the test set and computes
 %the norm against every vector in the training set
 N = 41;
 
+%Obtains our test and training set
 [test,train] = test_train_split(fullMatrix,41);
 
 
@@ -22,8 +23,10 @@ for i = 1:N
     userLength(3,i) = maximum;
 end
 
+
 matrixSize = size(test);
-averageSet = zeros(N, matrixSize(2) );
+%Create a blank matrix that will hold the average vector for a certain user 
+averageSet = zeros(N, matrixSize(2) );	
 
 
 v = zeros(1,matrixSize(2));
@@ -31,18 +34,24 @@ averageLength = length(userLength);
 for i = 1:length(averageSet(:,1))
 	
 
-
+	%Sum all the vectors for a certain user
 	for j = userLength(2,i) : userLength(3,i)
 
 		v = v + test(j,:);
 
 	end
-	v = v ./ userLength(1,i);
-	averageSet(i,:) = v;
-	v = zeros(1,matrixSize(2));
+
+	a = userLength(1,i);
+	v = v ./ userLength(1,i); %divide by the numbers of vectors
+	averageSet(i,:) = v;		%Assign to location i in th average set
+	v = zeros(1,matrixSize(2));	
 
 end
-a = 7;
+
+
+
+
+accuracy = averageSet;
 
 
 
