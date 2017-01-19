bf0a409fc121c771e290e36e977a4141cc538e39
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-01-18 21:35:44 -0500

    Extract column labels and normalize matrix

--- ML/simpleNorm.m
+++ ML/simpleNorm.m
@@ -1,6 +1,6 @@
 function [accuracy] = simpleNorm(fullMatrix)
 %This function takes a full data matrix, creates a 
-%testing and training set, and computes a simplie norm error,
+%testing and training set, and computes a simple norm error,
 %It averages the strokes for a user in the test set and computes
 %the norm against every vector in the training set
 N = 41;
@@ -8,6 +8,14 @@ N = 41;
 %Obtains our test and training set
 [test,train] = test_train_split(fullMatrix,41);
 
+
+testLabels = test(:,1:2);
+test(:,1:2) = [];
+test = normr(test);(polar)
+
+trainLabels = train(:,1:2);
+train(:,1:2) = [];
+train = normr(train);
 
 
 %Finds the nubmer of strokes for each user
@@ -24,73 +32,61 @@ for i = 1:N
 end
 
 
-% The following code finds the average vectors for users in the set
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%The following code finds the average vectors for users in the set
 
-matrixSize = size(test);
+matrixSize = size(test);    %rows by columns
+
 %Create a blank matrix that will hold the average vector for a certain user 
 averageSet = zeros(N, matrixSize(2) );	
 
 
-v = zeros(1,matrixSize(2));
-averageLength = length(userLength);	
-N = length(averageSet(:,1));
 for i = 1:N
-	
-	v = zeros(1,matrixSize(2));	
-
-	%Sum all the vectors for a certain user
-	for j = userLength(2,i) : userLength(3,i)
-
-		v = v + test(j,:);
-
-	end
-
-	a = userLength(1,i);
-	v = v ./ userLength(1,i); %divide by the numbers of vectors
-	averageSet(i,:) = v;		%Assign to location i in th average set
-	
+	userVectors = test(userLength(2,i):userLength(3,i), :);
+	userVectors = sum(userVectors);
+	userVectors = userVectors./userLength(1,i);
+	averageSet(i,:) = userVectors;
 end
 
-for i = 1:41
-	averageSet(i,1) = i;	
+averageSet(:,2) = [];
+train(:,2) = [];
+
+for i = 1:length(averageSet(:,1))
+	averageSet(i,1) = i;
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%Performing the norm calculation
-accuracyVector = zeros(1, 41);
+accuracyVector = zeros(1,41);
 accuracy = 0;
 minIndex = 0;
-M = length(averageSet(:,1));  %Length of the set of average vectors
-N = length(train(:,1));		%Length of the training set
-count = 0;
 
-%For every vector in the average test set
-for i = 1:M
-
-	minIndex = 1;
-	minNorm = 10^9;
+M = length(train(:,1));
+for i = 1:N
+	normMin = 10^9;
 	x = averageSet(i,:);
-
-	for j = 1:N
-		y = train(j,:);
-		normValue =  norm(x-y);
-		if (normValue < minNorm)
-			minNorm = normValue;
+	for j = 1: M
+		value = norm(x - train(j,:));
+		if (value < normMin)
+			normMin = value;
 			minIndex = j;
 		end
-
-
+	
 	end
+	
 
-	accuracyVector(i) = fullMatrix(minIndex,1);
+	accuracyVector(i) = train(minIndex,1);
 	if (i == accuracyVector(i))
 		accuracy = accuracy + 1;
-	end
-
-
-
-
 end
+
+
+
+
+
+
+
+
 
 l = 9;	
 
