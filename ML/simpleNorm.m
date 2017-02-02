08783c4ebe94dc793e88d5add1d0c2074ea0bee8
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-02-02 10:05:24 -0500

    Program classifying the k-smallest vectors per test vector, then taking the mode

--- ML/simpleNorm.m
+++ ML/simpleNorm.m
@@ -18,80 +18,24 @@ train(:,1:2) = [];
 train = normr(train);
 
 
-%Finds the nubmer of strokes for each user
-userLength = zeros(3,N); %stores the number of samples, starting index, and ending index for each user
-for i = 1:N
-    userIndex = find(testLabels(:,1) == i);      % Finds the indices of every row for a certain user
-    %Finds the minimum and maximum of indices length
-    minimum = min(userIndex);
-    maximum = max(userIndex);
-    
-    userLength(1,i) = maximum - minimum + 1;   
-    userLength(2,i) = minimum;
-    userLength(3,i) = maximum;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%The following code finds the average vectors for users in the set
-%{
-matrixSize = size(test);    %rows by columns
-
-%Create a blank matrix that will hold the average vector for a certain user 
-averageSet = zeros(N, matrixSize(2) );	
-
-
-for i = 1:N
-	userVectors = test(userLength(2,i):userLength(3,i), :);
-	userVectors = sum(userVectors);
-	userVectors = userVectors./userLength(1,i);
-	averageSet(i,:) = userVectors;
-end
-
-averageSet(:,2) = [];
-train(:,2) = [];
-
-for i = 1:length(averageSet(:,1))
-	averageSet(i,1) = i;
-end
-%}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 M = length(train(:,1));			%Number of vectors in the training set
 N = length(test(:,1));			%Number of vectors in the testing set
-
-accuracyVector = zeros(N,1);
+accuracyVector = zeros(1,N);
 accuracy = 0;
-minIndex = 0;
-
-
-for i = 1:N
-	normMin = 10^9;
+for i = 1:N   			%for every vector in the testing set
 	x = test(i,:);
-	for j = 1: M
-		value = norm(x - train(j,:));
-		if (value < normMin)
-			normMin = value;
-			minIndex = j;
-		end
-	
+	allNorms = zeros(M,2);		%create a vector that stores all norms for a certain test vector
+	for j = 1:M
+		allNorms(j,1) = j;
+		allNorms(j,2) = norm(x- train(j,:));
 	end
-	
 
-	accuracyVector(i) = trainLabels(minIndex,1);
-	if (testLabels(i,1) == accuracyVector(i))
+	[normMode, frequency] = NnearNeighbors(allNorms,149,trainLabels);
+	accuracyVector(i) = normMode;
+
+	if (accuracyVector(i) == testLabels(i,1))
 		accuracy = accuracy + 1;
-end
-
-
-
-
-
-
-
-
-
-l = 9;	
-
+	end
 
 end
